36ee19cd7d046c1c2df6c2af0ac1c3b44e0a4793
Ad1s <y.bryak@gmail.com>
2025-01-25 15:54:08 +0200

    Add CoroutinesHandler

---
+++ Assets/MBG/Develop/CommonServices/CoroutinesHandling/ICoroutinesHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.MBG.Develop.CommonServices.CoroutinesHandling
+{
+    public interface ICoroutinesHandler
+    {
+        public Coroutine StartRoutine(IEnumerator coroutine);
+        public void StopRoutine(Coroutine routine);
+    }
+}
